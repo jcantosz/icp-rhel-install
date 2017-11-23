1bb929e1a711e3dd61b2458ad95269a43723d837
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2017-11-23 13:11:54 -0500

    Yum update + docker install fallback

--- scripts/03-install-packages.sh
+++ scripts/03-install-packages.sh
@@ -10,7 +10,13 @@ source 00-variables.sh
 # Install docker & python on master
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum update -y
 sudo yum install -y docker-ce
+
+# Fall back to pinned version
+if [ "$?" == "1" ]; then
+  yum install --setopt=obsoletes=0 -y docker-ce-17.03.2.ce-1.el7.centos.x86_64 docker-ce-selinux-17.03.2.ce-1.el7.centos.noarch
+fi
 
 sudo yum install -y python-setuptools
 sudo easy_install pip
@@ -20,7 +26,13 @@ for ((i=0; i < $NUM_WORKERS; i++)); do
   # Install docker & python on worker
   ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo yum install -y yum-utils device-mapper-persistent-data lvm2
   ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo yum update -y
   ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo yum install -y docker-ce
+
+  # Fall back to pinned version
+  if [ "$?" == "1" ]; then
+    ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} yum install --setopt=obsoletes=0 -y docker-ce-17.03.2.ce-1.el7.centos.x86_64 docker-ce-selinux-17.03.2.ce-1.el7.centos.noarch
+  fi
 
   ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo yum install -y python-setuptools
   ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo easy_install pip
