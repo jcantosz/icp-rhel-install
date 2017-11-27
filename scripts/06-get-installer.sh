c5c095ea387249320d15a7c65936ee9af152341b
Jesse Antoszyk <jcantosz@us.ibm.com>
2017-11-27 10:41:59 -0500

    Add ppc architecture support

--- scripts/06-get-installer.sh
+++ scripts/06-get-installer.sh
@@ -3,10 +3,10 @@
 # Get the variables
 source 00-variables.sh
 
-sudo docker pull ibmcom/icp-inception:2.1.0
+sudo docker pull ibmcom/icp-inception${INCEPTION_TAG}:2.1.0
 
 sudo mkdir /opt/ibm-cloud-private-2.1.0
 sudo chown $USER /opt/ibm-cloud-private-2.1.0
 cd /opt/ibm-cloud-private-2.1.0
 
-sudo docker run -v $(pwd):/data -e LICENSE=accept ibmcom/icp-inception:2.1.0 cp -r cluster /data
+sudo docker run -v $(pwd):/data -e LICENSE=accept ibmcom/icp-inception${INCEPTION_TAG}:2.1.0 cp -r cluster /data
